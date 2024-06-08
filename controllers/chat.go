9ab136e4c7e49c6edb680b352d19d2552d0f3154
ahsmha <ahmed.5wna0@aleeas.com>
2024-06-08 23:23:49 +0530

    add webhooks for handling events

--- controllers/chat.go
+++ controllers/chat.go
@@ -1,17 +1,57 @@
 package controllers
 
-type ChatController struct{}
+import (
+	"net/http"
 
-func NewChatController() *ChatController {
-	return &ChatController{}
+	"github.com/SpoonBuoy/waba/models"
+	"github.com/SpoonBuoy/waba/service"
+	"github.com/gin-gonic/gin"
+)
+
+const systemAccessToken = "EAAMRwudSuNsBOwUijKcd1HmrCsL9twIZA5pFE8ASRB5S3yIdMiWVRRvOw2l5ZCumitduU4foUofA6CZBbEe6LcCYAiw4nsYZB1Ub6KsWihDBdGzg8m4E3UOBXW4KKC9bokSxV2inh9PvQMhIKxocBmY5Ja8uIucg5NIi3ZAxFst5xl2G24jFFA5pU082S19rp"
+const token = "EAAnrp3AqNGwBOz2ltZCOaeWBQ5OdDm04TH1mnIibbH8aZC0AoZBVB0g1BuLNwq96DsCfe5Ub9KWvKPGUkZAmvO8REmPVUAbKfcyNMTuhN4Jq9sLfwd29SuZBRH5ZBhMa8KfDnZAMRgxbg4wCYuJOYjrB435nL64YHIt3KuGc1LhqlZAu2AN3HXjNRPrCys581QgbA6YZCLZA4cko8tXA8giZAAZD"
+
+type ChatController struct {
+	service *service.WabaService
+}
+
+func NewChatController(service *service.WabaService) *ChatController {
+	return &ChatController{
+		service: service,
+	}
 }
 
 // verify api
-func (wbc *ChatController) Verify() {
+func (wbc *ChatController) Verify(c *gin.Context) {
+	// "/verify?hub.mode=subscribe&hub.challenge=1291346053&hub.verify_token=bot-test"
+	mode := c.Query("hub.mode")
+	challenge := c.Query("hub.challenge")
+	verifyToken := c.Query("hub.verify_token")
 
+	if mode == "subscribe" && verifyToken == "bot-test" {
+		c.String(http.StatusOK, challenge)
+	} else {
+		c.AbortWithStatus(http.StatusUnauthorized)
+	}
 }
 
 // webhook listening new message events
-func (wbc *ChatController) Listen() {
+func (wbc *ChatController) Listen(c *gin.Context) {
+	var body models.WAMessagePayload
+	if err := c.BindJSON(&body); err != nil {
+		c.Error(err)
+		return
+	}
 
+	if len(body.Entry) > 0 && len(body.Entry[0].Changes) > 0 && len(body.Entry[0].Changes[0].Value.Messages) > 0 && body.Entry[0].Changes[0].Field == "messages" {
+		if err := wbc.service.handle(&body); err != nil {
+			c.Error(err)
+			return
+		}
+	} else {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Payload type not supported",
+		})
+		return
+	}
 }
--- models/request.dto.go
+++ models/request.dto.go
@@ -1,1 +1,67 @@
 package models
+
+type MessageType string
+
+const (
+	BUTTON   MessageType = "button"
+	DOCUMENT MessageType = "document"
+	TEXT     MessageType = "text"
+)
+
+// message request
+type WAMessagePayload struct {
+	Object string  `json:"object"`
+	Entry  []Entry `json:"entry"`
+}
+
+type Entry struct {
+	ID      string   `json:"id"`
+	Changes []Change `json:"changes"`
+}
+
+type Change struct {
+	Field string `json:"field"`
+	Value struct {
+		MessagingProduct string      `json:"messaging_product"`
+		Metadata         Metadata    `json:"metadata"`
+		Contacts         []Contact   `json:"contacts"`
+		Messages         []WAMessage `json:"messages"`
+	} `json:"value"`
+}
+
+type Metadata struct {
+	DisplayPhoneNumber string `json:"display_phone_number"`
+	PhoneNumberID      string `json:"phone_number_id"`
+}
+
+type Contact struct {
+	Profile struct {
+		Name string `json:"name"`
+	} `json:"profile"`
+	WaID string `json:"wa_id"`
+}
+
+type WAMessage struct {
+	From      string      `json:"from"`
+	ID        string      `json:"id"`
+	Timestamp int64       `json:"timestamp"`
+	Text      string      `json:"text"`
+	Type      MessageType `json:"type"` // only can be - button, document, text
+	Document  *Document   `json:"document"`
+	Button    *Button     `json:"button"`
+}
+
+type Document struct {
+	Filename         string `json:"filename"`
+	MimeType         string `json:"mime_type"`
+	Sha256           string `json:"sha256"`
+	ID               string `json:"id"`
+	MessagingProduct string `json:"messaging_product"`
+}
+
+type Button struct {
+	Type    string  `json:"type"`
+	Text    string  `json:"text"`
+	Payload *string `json:"payload"`
+	URL     *string `json:"url"`
+}
